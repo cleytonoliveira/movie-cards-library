621a67043e020b968b214cfc91872f2fcff53fe8
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-12-19 00:53:04 -0300

    Updates

---
+++ precache-manifest.89a90f27dbc580e209dc7a497d865039.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d60ea4dc139941d61da8f2d6c1810d59",
+    "url": "/movie-cards-library/index.html"
+  },
+  {
+    "revision": "8c6a5db512084c1fa610",
+    "url": "/movie-cards-library/static/css/main.6f262133.chunk.css"
+  },
+  {
+    "revision": "ce03969a07b46381b5df",
+    "url": "/movie-cards-library/static/js/2.838aab56.chunk.js"
+  },
+  {
+    "revision": "8c6a5db512084c1fa610",
+    "url": "/movie-cards-library/static/js/main.fad4dd13.chunk.js"
+  },
+  {
+    "revision": "ae17a1ff270632977288",
+    "url": "/movie-cards-library/static/js/runtime-main.c2f7205f.js"
+  }
+]);
